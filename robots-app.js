0003aecef0b1786ce3c576607641244bec3fc02a
Erica Kirshner <ekkicks@gmail.com>
2017-08-22 16:54:43 -0400

    finished project

--- robots-app.js
+++ robots-app.js
@@ -12,22 +12,62 @@ server.use(bodyparser.urlencoded({ extended: false }));
 server.engine('mustache', mustache());
 server.set('views', './views');
 server.set('view engine', 'mustache');
+//css
+server.use(express.static('views'));
 
 
-mongo.connect('monogodb://localhost:27017/robotDatabase', function (err, database) {
+mongo.connect('mongodb://localhost:27017/robotdatabase', function (err, db) {
     let robots = db.collection('robots');
 
-    server.get('/', function(req,res) {
-        robots.find().toArray(function (err, robotsData) { 
+    server.get('/', function (req, res) {
+
+        robots.find().toArray().then(function (robotUsers) {
+            console.log(robotUsers[0].address.country)
+
             res.render('index', { 
-                users: robotsData
-            })
+                users: robotUsers
+            });
         });
     });
+
+// creating new path for job
+    server.get('/employed', function (req, res) {
+        
+        robots.find({ job: /.+/ }).toArray().then(function (robotUsers) {
+            res.render('index', { 
+                users: robotUsers
+            });
+        });
+    });
+
+//creating new path for no job
+server.get('/looking', function (req, res) {
+
+        robots.find({ job: null }).toArray().then(function (robotUsers) {
+            res.render('index', { 
+                users: robotUsers
+            });
+        });
+    });
+
+
+//creating new path for country
+server.get('/country/:country', function (req, res) {
+
+        robots.find({ 'address.country': req.params.country }).toArray().then(function (robotUsers) {
+
+            res.render('index', { 
+                users: robotUsers
+            });
+        });
+    });    
+
 
     // server
     server.listen(3000, function () {
         console.log('woot woot');
     });
 });
+
+
 
